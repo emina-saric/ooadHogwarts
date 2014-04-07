3f91a13457e3142759df6e44150c6c752af06188
Wolverine2309 <arminija@hotmail.com>
2014-04-07 21:11:32 +0200

    Armin Klacar
    Ucenik,Program,Studentska Sluzba

---
+++ Hogwarts2/ConsoleApplication1/StudentskaSluzba.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class StudentskaSluzba
+    {
+        public StudentskaSluzba() { }
+        
+        List<Ucenik> _ucenici = new List<Ucenik>();
+        List<Profesor> _profesori = new List<Profesor>();
+        List<Ucenik> _prefekti = new List<Ucenik>();
+        List<Osoba> _clanovi_studentske = new List<Osoba>();
+        List<Ucenik> _kapiteni = new List<Ucenik>();
+        List<Profesor> _predstavnik_kuce = new List<Profesor>();
+        List<Predmet> _predmeti = new List<Predmet>();
+        List<Kuca> _kuce = new List<Kuca>();
+
+
+    }
+}
---
+++ Hogwarts2/ConsoleApplication1/Ucenik.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Ucenik : Osoba
+    {
+        Ucenik(string ime, string prezime, DateTime datum_rodjenja, string username, string password, int godina_studija, Kuca _kuca,bool prefekt)
+            : base(ime, prezime, datum_rodjenja, username, password)
+        {
+            Godina_studija = godina_studija;
+            kuca = _kuca;
+            Prefekt = prefekt;
+        }
+        int Godina_studija { get; set; }
+        Kuca kuca { get; set; }
+        bool Prefekt { get; set; }
+        
+        public void PovecajGodinuStudija()
+        {
+            if (Godina_studija > 1 && Godina_studija < 7) Godina_studija++;
+        }
+        public void PostaviZaPrefekta()
+        {
+            if (Godina_studija > 4) Prefekt = true;
+        }
+        public string DajKucu() 
+        {
+            return kuca.Ime_kuce;
+        }
+    }
+}
