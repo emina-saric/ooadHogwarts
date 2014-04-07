e56c0da59b46fef1b2fb5f540a67a0d687369ce3
Wolverine2309 <arminija@hotmail.com>
2014-04-07 22:13:26 +0200

    Armin Klacar
    Osoba

---
+++ Hogwarts2/ConsoleApplication1/Osoba.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Osoba
+    {
+       public Osoba(string ime, string prezime, DateTime datum_rodjenja, string username, string password) {
+            Ime = ime;
+            Prezime = prezime;
+            Username = username;
+            Password = password;
+            Datum_rodjenja = datum_rodjenja;
+        }
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public DateTime Datum_rodjenja { get; set; }
+    }
+}
