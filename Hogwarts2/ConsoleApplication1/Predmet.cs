47cec725dc2c79daaa83d26141e8c737b4e84a0e
LavaGolem <irma.jakic@gmail.com>
2014-04-08 11:09:46 +0200

    fg
    fgd

--- Hogwarts2/ConsoleApplication1/Predmet.cs
+++ Hogwarts2/ConsoleApplication1/Predmet.cs
@@ -7,13 +7,19 @@ namespace ConsoleApplication1
 {
     class Predmet
     {
-        Predmet(string naziv, Profesor profesor = null)
+        Predmet(string naziv, int casovi, Profesor profesor = null)
         {
             Naziv = naziv;
+            Broj_casova = casovi;
             if (profesor != null) Predmetni_profesor = profesor;
 
         }
         string Naziv { get; set; }
         Profesor Predmetni_profesor { get; set; }
+        int Broj_casova { get; set; }
+        public void PromjeniProfesora(Profesor profesor)
+        {
+            Predmetni_profesor = profesor;
+        }
     }
 }
