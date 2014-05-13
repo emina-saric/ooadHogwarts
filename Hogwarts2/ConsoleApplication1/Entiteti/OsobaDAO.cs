d5753cff6dea92f23e71954a83347c461a3f6cb3
Wolverine2309 <arminija@hotmail.com>
2014-05-13 15:45:06 +0200

    Armin
    Test

---
+++ Hogwarts2/ConsoleApplication1/Entiteti/OsobaDAO.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace DAL
+{
+    public partial class DAL
+    {
+        public class OsobaDAO : IDaoCrud<Osoba>
+        {
+            protected MySqlCommand c;
+
+            public long create(Osoba entity)
+            {
+                try
+                {
+                    c = new MySqlCommand("insert into osobe values ('" + entity.Ime + "','" + entity.Prezime + ","
+                        +entity.Datum_rodjenja+","+entity.Username+","+entity.Password+"')", con);
+                    c.ExecuteNonQuery();
+                    return c.LastInsertedId;
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
+            }
+
+            public Osoba read(Osoba entity)
+            {
+                c = new MySqlCommand("select * from osobe where ime=" + entity.Ime + " and prezime=" + entity.Prezime + " and datum_rodjenja=" + entity.Datum_rodjenja
+                    + " and username=" + entity.Username + " and password=" + entity.Password + ";");
+                MySqlDataReader mr = c.ExecuteReader();
+                if (mr.Read())
+                    return new Osoba(mr.GetString("ime"), mr.GetString("prezime"), mr.GetDateTime("datum_rodjenja"), mr.GetString("username"), mr.GetString("password"));
+                else
+                    return null;
+            }
+
+            public Osoba update(Osoba entity)
+            {
+                c = new MySqlCommand("update osobe set ime="+entity.Ime+", prezime="+entity.Prezime +", datum_rodjenja="+ entity.Datum_rodjenja
+                    + ", password=" + entity.Password + "where username="+entity.Username+";");
+                c.ExecuteNonQuery();
+                return read(entity);
+            }
+
+            public void delete(Osoba entity)
+            {
+                c = new MySqlCommand("delete osobe where ime=" + entity.Ime + " and prezime=" + entity.Prezime + " and datum_rodjenja=" + entity.Datum_rodjenja
+                    + " and username=" + entity.Username + " and password=" + entity.Password + ";");
+                c.ExecuteNonQuery();
+            }
+
+            public Osoba getById(int id)
+            {
+                c = new MySqlCommand("select * from osobe where id="+Convert.ToString(id)+";");
+                MySqlDataReader mr = c.ExecuteReader();
+                if (mr.Read())
+                    return new Osoba(mr.GetString("ime"), mr.GetString("prezime"), mr.GetDateTime("datum_rodjenja"), mr.GetString("username"), mr.GetString("password"));
+                else
+                    return null;
+                 
+            }
+
+            public List<Osoba> getAll()
+            {
+                try
+                {
+                    c = new MySqlCommand("select * from osobe", con);
+                    MySqlDataReader mr = c.ExecuteReader();
+                    List<Osoba> osobe = new List<Osoba>();
+                    while (mr.Read())
+                        osobe.Add(new Osoba(mr.GetString("ime"), mr.GetString("prezime"), mr.GetDateTime("datum_rodjenja"), mr.GetString("username"), mr.GetString("password")));
+                    return osobe;
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
+            }
+
+            public List<Osoba> getByExample(string ime, string prezime)
+            {
+                try
+                {
+                    c = new MySqlCommand("select * from osobe where ime="+ime+" and prezime="+prezime, con);
+                    MySqlDataReader mr = c.ExecuteReader();
+                    List<Osoba> osobe = new List<Osoba>();
+                    while (mr.Read())
+                        osobe.Add(new Osoba(mr.GetString("ime"), mr.GetString("prezime"), mr.GetDateTime("datum_rodjenja"), mr.GetString("username"), mr.GetString("password")));
+                    return osobe;
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
+            }
+        }   
+    }
+}
---
+++ Hogwarts2/ConsoleApplication1/Entiteti/PredmetDAO.cs
@@ -0,0 +1,98 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public partial class DAL
+    {
+        class PredmetDAO : IDaoCrud<Predmet>
+        {
+            protected MySqlCommand c;
+            public long create(Predmet entity)
+            {
+                try
+                {
+                    c = new MySqlCommand("insert into predmet values ('" + entity.Naziv + "','" + entity.Broj_casova + ","
+                        + entity.Id_profesor +"')", con);
+                    c.ExecuteNonQuery();
+                    return c.LastInsertedId;
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
+            }
+
+            public Predmet read(Predmet entity)
+            {
+                c = new MySqlCommand("select * from predmet where id_predmet=" + entity.Id_predmet + "and ime=" + entity.Naziv + " and br_cas=" + entity.Broj_casova + " and id_prof=" + entity.Id_profesor + ";");
+                MySqlDataReader mr = c.ExecuteReader();
+                if (mr.Read())
+                    return new Predmet(mr.GetInt32("id_predmet"), mr.GetString("ime"), mr.GetInt32("br_cas"), mr.GetInt32("id_prof"));
+                else
+                    return null;
+            }
+
+            public Predmet update(Predmet entity)
+            {
+                c = new MySqlCommand("update predmet set id_predmet=" + entity.Id_predmet + "and ime=" + entity.Naziv + " and br_cas=" + entity.Broj_casova + " and id_prof=" + entity.Id_profesor + ";");
+                c.ExecuteNonQuery();
+                return read(entity);
+            }
+
+            public void delete(Predmet entity)
+            {
+                c = new MySqlCommand("delete predmet where id_predmet=" + entity.Id_predmet + "and ime=" + entity.Naziv + " and br_cas=" + entity.Broj_casova + " and id_prof=" + entity.Id_profesor + ";");
+                c.ExecuteNonQuery();
+            }
+
+            public Predmet getById(int id)
+            {
+                c = new MySqlCommand("select * from predmet where id=" + Convert.ToString(id) + ";");
+                MySqlDataReader mr = c.ExecuteReader();
+                if (mr.Read())
+                    return new Predmet(mr.GetInt32("id_predmet"),mr.GetString("ime"), mr.GetInt32("br_cas"), mr.GetInt32("id_prof"));
+                else
+                    return null;
+            }
+
+            public List<Predmet> getAll()
+            {
+                try
+                {
+                    c = new MySqlCommand("select * from predmet", con);
+                    MySqlDataReader mr = c.ExecuteReader();
+                    List<Predmet> _predmeti = new List<Predmet>();
+                    while (mr.Read())
+                        _predmeti.Add(new Predmet(mr.GetInt32("id_predmet"), mr.GetString("ime"), mr.GetInt32("br_cas"), mr.GetInt32("id_prof")));
+                    return _predmeti;
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
+            }
+
+            public List<Predmet> getByExample(string name, string value)
+            {
+                try
+                {
+                    c = new MySqlCommand("select * from predmet where ime=" + name  , con);
+                    MySqlDataReader mr = c.ExecuteReader();
+                    List<Predmet> _predmeti = new List<Predmet>();
+                    while (mr.Read())
+                        _predmeti.Add(new Predmet(mr.GetInt32("id_predmet"), mr.GetString("ime"), mr.GetInt32("br_cas"), mr.GetInt32("id_prof")));
+                    return _predmeti;
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
+            }
+        }
+    }
+}
